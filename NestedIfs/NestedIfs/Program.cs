8082c4a686eedc659e84dfd87fbe407c7b9590ea
David Barnes <david.barnes115@gmail.com>
2019-10-23 09:42:11 -0400

    pracice with if statements/ternary operators

---
+++ NestedIfs/NestedIfs/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace NestedIfs
+{
+    class MainClass
+    {
+
+        static string userName;
+        static string password;
+
+
+        public static void Main(string[] args)
+        {
+
+            Register();
+            Login();
+
+            Console.Read();
+
+
+        }
+
+        public static void Register()
+        {
+
+            Console.WriteLine("Please register by entering a username");
+            userName = Console.ReadLine();
+            Console.WriteLine("Please entere a password");
+            password = Console.ReadLine();
+            Console.WriteLine("Registration completed");
+            Console.WriteLine("---------------------------------------");
+        }
+
+        public static void Login()
+        {
+            Console.WriteLine("Please enter your username");
+
+            if(userName == Console.ReadLine())
+            {
+                Console.WriteLine("Please enter your password");
+                if(password == Console.ReadLine())
+                {
+                    Console.WriteLine("Login successful");
+                }
+                else
+                {
+                    Console.WriteLine("Login failed.  Wrong Password. Restart program.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Login failed.  Wrong Username. Restart program.");
+            }
+        }
+    }
+}
---
+++ TernaryOperator/TernaryOperator/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace TernaryOperator
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+
+            int temperature = -5;
+            string stateOfMatter;
+
+            if (temperature < 0)
+            {
+                stateOfMatter = "solid";
+            }
+            else
+            {
+                stateOfMatter = "liquid";
+            }
+
+            Console.WriteLine("State of matter is {0}", stateOfMatter);
+
+            temperature += 200;
+
+            //stateOfMatter = temperature < 0 ? "solid" : "liquid";
+
+            stateOfMatter = temperature > 100 ? "gas" : temperature < 0 ? "solid" : "liquid";
+
+            Console.WriteLine("State of matter is {0}", stateOfMatter);
+            Console.ReadKey();
+
+        }
+    }
+}
