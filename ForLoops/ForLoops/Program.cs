e667917569f10869ea71adc436f09d3549873b5f
David Barnes <david.barnes115@gmail.com>
2019-10-23 09:40:56 -0400

    adding practice files

---
+++ ForLoops/ForLoops/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ForLoops
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int lengthOfText = 0;
+            string wholeText = "";
+
+            do
+            {
+                Console.WriteLine("Please enter the name of a friend");
+                string nameOfFriend = Console.ReadLine();
+                int currentLength = nameOfFriend.Length;
+                lengthOfText += currentLength;
+                wholeText += nameOfFriend;
+            } while (lengthOfText < 20);
+            Console.WriteLine("Thanks, that was enough " + wholeText);
+
+            Console.Read();
+        }
+    }
+}
