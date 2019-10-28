114f540c47532a942fa1819e3c85d0f5f4fdc364
David Barnes <david.barnes115@gmail.com>
2019-10-28 10:20:02 -0400

    solution for "average challenge"

---
+++ AverageChallenge/AverageChallenge/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace AverageChallenge
+{
+    class MainClass
+
+    {
+
+        static double runningScoresTotal;
+
+        public static void Main(string[] args)
+        {
+
+            int numCurrentScore = 0;
+            int counter = 0;
+            string currentScore = "";
+
+            while (numCurrentScore != -1)
+            {
+                Console.WriteLine("Please enter a student score.");
+                currentScore = Console.ReadLine();
+
+                if (currentScore != "-1")
+                {
+                    numCurrentScore = int.Parse(currentScore);
+                    runningScoresTotal += numCurrentScore;
+                    counter++;
+                    continue;
+                }
+
+                break;
+            }
+
+            double averageScore = runningScoresTotal / counter;
+
+            Console.WriteLine(averageScore);
+
+
+
+
+        }
+
+        
+    }
+}
