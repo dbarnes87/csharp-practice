96402647ca1e83ff35e2aa5195742f432a37615f
David Barnes <david.barnes115@gmail.com>
2019-10-28 11:58:11 -0400

    practice with classes, objects, constructors

---
+++ Classes___Basics/Classes___Basics/Human.cs
@@ -0,0 +1,67 @@
+﻿using System;
+namespace Classes___Basics
+{
+    // this class is a blueprint of a datatype
+    public class Human
+    {
+        // member variable
+        private string firstName;
+        private string lastName;
+        private string eyeColor;
+        private int age;
+
+        // default constructor
+        public Human()
+        {
+            Console.WriteLine("Constructor called.  Object created.");
+        }
+
+        public Human(string firstName)
+        {
+            this.firstName = firstName;
+        }
+
+        public Human(string firstName, string lastName)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+
+        // parameterized constructor
+        public Human(string firstName, string lastName, int age)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.age = age;
+        }
+
+        public Human(string firstName, string lastName, string eyeColor)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.eyeColor = eyeColor; 
+        }
+
+        // parameterized constructor
+        public Human(string firstName, string lastName, string eyeColor, int age)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.eyeColor = eyeColor;
+            this.age = age;
+        }
+
+        // member method
+        public void IntroduceMyself()
+        {
+            if (age != 0 && lastName != null && eyeColor != null && firstName != null)
+                Console.WriteLine("Hi, I'm {0} {1} and am {2} years old.  My eyes are {3}.", firstName, lastName, age, eyeColor);
+            else if (age != 0 && lastName != null && firstName != null)
+                Console.WriteLine("Hi, I'm {0} {1} and am {2} years old.", firstName, lastName, age);
+            else if (lastName != null && firstName != null)
+                Console.WriteLine("Hi, I'm {0} {1}.", firstName, lastName);
+            else
+                Console.WriteLine("Hi, I'm {0}.", firstName);
+        }
+    }
+}
---
+++ Classes___Basics/Classes___Basics/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Classes___Basics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Human ross = new Human("Ross", "Geller", "brown", 35);
+            ross.IntroduceMyself();
+
+            Human rachel = new Human("Rachel", "Greene", "blue", 34);
+            rachel.IntroduceMyself();
+
+            Human emma = new Human("Emma", "Geller-Greene", 2);
+            emma.IntroduceMyself();
+
+            Human joey = new Human("Joey", "Tribianni");
+            joey.IntroduceMyself();
+
+            Human chandler = new Human("Chandler");
+            chandler.IntroduceMyself();
+
+            Human basicHuman = new Human();
+            basicHuman.IntroduceMyself();
+
+            Console.ReadKey();
+        }
+    }
+}
